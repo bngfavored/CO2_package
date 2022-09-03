dc5a15486a083b340bbfcc39d78071b8023a73f0
Richard Chevious <bngfavored@gmail.com>
2022-09-02 19:00:11 -0500

    Updated docker_entrypoint.sh to connect to MAC address

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -2,6 +2,10 @@
 
 service dbus start
 bluetoothd &
+hcitool scan
+bluetoothctl trust 60:C0:BF:48:38:5F
+bluetoothctl pair 60:C0:BF:48:38:5F
+bluetoothctl connect 60:C0:BF:48:38:5F
 python ./main.py
 
 /bin/bash
