dca857a55d211351c74fb91404195e532e46a288
Richard Chevious <bngfavored@gmail.com>
2022-09-03 18:50:21 -0500

    committed out python ./main.py

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -8,7 +8,7 @@ bluetoothctl scan off
 bluetoothctl trust 60:C0:BF:48:38:5F
 bluetoothctl pair 60:C0:BF:48:38:5F
 bluetoothctl connect 60:C0:BF:48:38:5F
-python ./main.py
+#python ./main.py
 
 /bin/bash
 
