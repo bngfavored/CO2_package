7454e9fed8d1a7b3932db511b16d853e707db1cb
Richard Chevious <bngfavored@gmail.com>
2022-09-03 18:44:10 -0500

    Updated docker_entrypoint.sh with bluetoothctl scan on/off

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 service dbus start
-bluetoothd &
+service bluetooth start
 hcitool scan
+bluetoothctl scan on
+bluetoothctl scan off
 bluetoothctl trust 60:C0:BF:48:38:5F
 bluetoothctl pair 60:C0:BF:48:38:5F
 bluetoothctl connect 60:C0:BF:48:38:5F
 python ./main.py
 
 /bin/bash
+
+
